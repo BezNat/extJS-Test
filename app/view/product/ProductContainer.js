a827f68f273381deff5264c1250836c734033dec
BezNat <nsb2010@list.ru>
2025-09-10 11:56:11 +0300

    Update ProductContainer.js

--- app/view/product/ProductContainer.js
+++ app/view/product/ProductContainer.js
@@ -5,7 +5,6 @@ Ext.define('MyApp.view.product.ProductContainer', {
     height: '100%',
     record: null,
     initComponent: function() {
-        console.log('ProductContainer record:', this.record);
         var me = this;
         me.items = [Ext.create('MyApp.view.product.ProductForm', {record: me.record})
         ];
